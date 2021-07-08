b23e0afd69d5cf8937ae99b5efa57bf700195aa5
SwissDaVinci_HD <simon.lissa@sdvware.net>
2021-07-09 00:06:50 +0200

    Add Ping Pong

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-// index.js
+// require the discord.js module
 const Discord = require('discord.js');
+
+// create a new Discord client
 const client = new Discord.Client();
 
-// ... client setup (keep reading)
-console.log(process.env.A);
-console.log(process.env.B);
-client.login(process.env.TOKEN);
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Ping Pong Test
+client.on('message', message => {
+	if (message.content === '§ping') {
+		// send back "Pong." to the channel the message was sent in
+		message.channel.send('Pong.');
+	}
+});
+
+// login to Discord with your app's token
+client.login('ODYyNzk0MzI1MTg4MDE4MjA3.YOdh1Q.aIH9ivxARzKbKcaA-4bHThxkFJI');
