269fea02125d3e9c83eb34e20e8aedefe45aa7c7
SwissDaVinci_HD <simon.lissa@sdvware.net>
2021-07-10 15:29:46 +0200

    add function to read modul files

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
 // require the config
-const { prefix, token } = require('./config.json');
+const { prefix, token, twitch, live_title, stream_status, modules } = require('./config.json');
+
 
 // require the discord.js module
 const Discord = require('discord.js');
 
+// require the node file system "fs"
+const fs = require('fs');
+
 // create a new Discord client
 const client = new Discord.Client();
+
+// create a new Discord Collection
+client.commands = new Discord.Collection();
+
+// read the Modulesfiles in ./bot_modules
+const commandFiles = fs.readdirSync(modules).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./bot_modules/${file}`);
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', () => {
 	console.log('Ready!');
-	client.user.setStatus('available');
 	client.user.setPresence({
-		game: {
-			name: 'with depression',
-			type: 'STREAMING',
-			url: 'https://www.twitch.tv/swissdavinci_hd',
+		status: 'online',
+		activity: {
+			name: live_title,
+			type: stream_status,
+			url: twitch,
 		},
 	});
 });
 
 // Ping Pong Test
 client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(' ');
+	const command = args.shift().toLowerCase();
+
 	if (message.content === `${prefix}ping`) {
 		message.channel.send('Pong');
 	}
@@ -32,10 +54,14 @@ client.on('message', message => {
 	else if (message.content === `${prefix}server`) {
 		message.channel.send(`Servername: ${message.guild.name}`);
 	}
+	else if (command === 'args-info') {
+		if (!args.length) {
+			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+		}
+
+		message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+	}
 });
-
-// import other Modules
-
 
 // login to Discord with your app's token
 client.login(token);
